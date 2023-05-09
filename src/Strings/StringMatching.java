9783433cd03ce5f645d3e30d3c2bf085103bab8d
Prateek <prateeksingh3212@gmail.com>
2023-05-09 09:12:55 +0530

    String matching algo...

---
+++ src/Strings/StringMatching.java
@@ -0,0 +1,141 @@
+package Strings;
+
+import java.util.*;
+
+public class StringMatching {
+    public static void main(String[] args) {
+        /**
+         * String Matching Alog....
+         * 1. Naive Algo
+         * 2. Rabin Karp
+         * 3. KMP (Kruth Morris)
+         * */
+
+        String str = "aabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcfdsssssssssssssssssssssssssssssssssssssssssddddddddddddddddddddddddffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcbababcabcabcabcaabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcbcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcaabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcababcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabccabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcbcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabccabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabababdsdsdsddassssssssssssssssssssssssssssssssssssssas";
+        String match = "ababd";
+
+        long time = System.nanoTime();
+        int ans = naive(str, match);
+        System.out.println("NAIVE ----> timeDiff: " + (System.nanoTime()-time) + " ans: " + ans);
+
+        time = System.nanoTime();
+        ans = rabinKarp(str, match);
+        System.out.println("RABIN KARP ----> timeDiff: " + (System.nanoTime()-time) + " ans: " + ans);
+
+        time = System.nanoTime();
+        ans = kmp(str, match);
+        System.out.println("KMP -----> timeDiff: " + (System.nanoTime()-time) + " ans: " + ans);
+    }
+
+    private static int naive(String str, String match) {
+        char[] strArr = str.toCharArray();
+        char[] matchArr = match.toCharArray();
+
+        for(int i=0; i<strArr.length; i++) {
+            int temp = i;
+            for(int j=0; j<matchArr.length; j++) {
+                if(i>= strArr.length) {
+                    return -1;
+                }
+                if(strArr[i] != matchArr[j]) {
+                    i = temp;
+                    break;
+                }
+                if(j+1==matchArr.length) {
+                    return temp;
+                }
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    private static int rabinKarp(String str, String match) {
+        if (match.length() == 0) {
+            return -1;
+        }
+
+        long hash = findHash(match.toCharArray());
+        long currHash = 0;
+
+        for (int i=0; i<str.length(); i++) {
+            if(i<match.length()) {
+                currHash = currHash * 100 + (122-str.charAt(i));
+                continue;
+            }
+            if(hash == currHash) {
+                return i - match.length();
+            }else {
+                currHash = nextRollingHash(currHash, str.charAt(i-match.length()), str.charAt(i), match.length());
+            }
+        }
+
+        if(hash == currHash) {
+            return str.length() - match.length();
+        }
+        return -1;
+    }
+
+    private static int kmp(String str, String match) {
+        if (match.length() == 0) {
+            return -1;
+        }
+
+        int[] piValue = calculatePITable(match);
+        int j = -1;
+
+        for(int i=0; i<str.length(); ) {
+            if (j == match.length()-1) {
+                return i-j-1;
+            }
+            if(str.charAt(i) == match.charAt(j+1)) {
+                j++;
+                i++;
+                continue;
+            }
+
+            if(j>0){
+                j = piValue[j]-1;
+            }else {
+                i++;
+            }
+        }
+
+        if (j == match.length()-1) {
+            return str.length()-j-1;
+        }
+
+        return -1;
+    }
+
+    private static long findHash(char[] arr) {
+        long hash = 0;
+        for (char c : arr) {
+            hash = hash * 100 + (122-c);
+        }
+        return hash;
+    }
+
+    private static long nextRollingHash(long currHash, char lastChar, char newChar, int matchLength) {
+        return (currHash - ((122 - lastChar) * (long) Math.pow(100, matchLength-1)))*100 + (122 - newChar);
+    }
+
+    private static int[] calculatePITable(String match) {
+        Map<Character, Integer> tempMap = new HashMap<>();
+        int[] piValue = new int[match.length()];
+
+        for(int i=0; i<match.length(); i++) {
+            char currChar = match.charAt(i);
+            if (tempMap.containsKey(currChar)) {
+                int index = tempMap.get(currChar);
+                piValue[i] = index;
+                tempMap.put(currChar, i+1);
+            }else {
+                tempMap.put(currChar, i+1);
+                piValue[i] = 0;
+            }
+        }
+
+        return piValue;
+    }
+}
